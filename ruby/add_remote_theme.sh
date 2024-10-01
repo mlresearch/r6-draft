48fdecac4161b354af776b236d5ec2fb6ac65f7a
Kento Nozawa <nzw0301@preferred.jp>
2024-10-02 01:59:13 +0900

    - remove ruby and haskell dirs - download pdfs - init top level bibtex file

--- ruby/add_remote_theme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git pull
-../papersite/ruby/update_volume.rb
-rm -rf _layouts/ _includes/ bibliography.bib feed.xml citeproc.yaml 
-git add README.md Gemfile
-git commit -a -m "Update with remote theme"
-git push
-
